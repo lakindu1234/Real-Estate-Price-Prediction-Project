c13e784807b9a99b11e11bf67c8ba1492149b4f0
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-08-21 22:43:28 +0530

    add to UI

--- client/app.js
+++ client/app.js
@@ -1,6 +1,7 @@
 function onPageLoad() {
     console.log('document loaded');
-    // var url = "http://127.0.0.1:5000/get_location_names"; // Use this if you are NOT using nginx which is first 7 tutorials
+
+    var url = 'http://127.0.0.1:5000/get_location_names'; // Use this if you are NOT using nginx which is first 7 tutorials
     var url = '/api/get_location_names'; // Use this if  you are using nginx. i.e tutorial 8 and onwards
     $.get(url, function (data, status) {
         console.log('got response for get_location_names request');
