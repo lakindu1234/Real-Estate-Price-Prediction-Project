99a44ac800ba599072094518de83820951315717
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-08-21 22:48:36 +0530

    function onClickedEstimatePrice() { add

--- client/app.js
+++ client/app.js
@@ -1,3 +1,37 @@
+
+
+
+
+
+
+
+function onClickedEstimatePrice() {
+    console.log('Estimate price button clicked');
+    var sqft = document.getElementById('uiSqft');
+    var bhk = getBHKValue();
+    var bathrooms = getBathValue();
+    var location = document.getElementById('uiLocations');
+    var estPrice = document.getElementById('uiEstimatedPrice');
+
+    // var url = "http://127.0.0.1:5000/predict_home_price"; //Use this if you are NOT using nginx which is first 7 tutorials
+    var url = '/api/predict_home_price'; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+
+    $.post(
+        url,
+        {
+            total_sqft: parseFloat(sqft.value),
+            bhk: bhk,
+            bath: bathrooms,
+            location: location.value,
+        },
+        function (data, status) {
+            console.log(data.estimated_price);
+            estPrice.innerHTML = '<h2>' + data.estimated_price.toString() + ' Lakh</h2>';
+            console.log(status);
+        }
+    );
+}
+
 function onPageLoad() {
     console.log('document loaded');
 
