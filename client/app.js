1fbb5799ad00b9bceb8f2b199a0fcff20c0d2cb2
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-08-21 22:46:37 +0530

    add to UI part in html all cities

--- client/app.js
+++ client/app.js
@@ -2,12 +2,14 @@ function onPageLoad() {
     console.log('document loaded');
 
     var url = 'http://127.0.0.1:5000/get_location_names'; // Use this if you are NOT using nginx which is first 7 tutorials
-    var url = '/api/get_location_names'; // Use this if  you are using nginx. i.e tutorial 8 and onwards
+
     $.get(url, function (data, status) {
         console.log('got response for get_location_names request');
         if (data) {
             var locations = data.locations;
+
             var uiLocations = document.getElementById('uiLocations');
+
             $('#uiLocations').empty();
             for (var i in locations) {
                 var opt = new Option(locations[i]);
@@ -16,3 +18,5 @@ function onPageLoad() {
         }
     });
 }
+
+window.onload = onPageLoad;
